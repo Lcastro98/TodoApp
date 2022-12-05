de717f20bdbc0adc785498958fa1a4ebeb509300
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-12-05 10:20:15 -0500

    Combinando 2 o mas reducers en el AppState

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,5 +1,15 @@
+import { ActionReducerMap } from "@ngrx/store";
+import { filtroReducer } from "./filtro/filtro.reducer";
+import { filtrosValidos } from './filtro/filtro.actions';
 import { Todo } from "./todos/models/todo.model";
+import { todoReducer } from "./todos/todo.reducer";
 
 export interface AppState {
-    todos: Todo[]
+    todos: Todo[],
+    filtro: filtrosValidos
+}
+
+export const appReducers: ActionReducerMap<AppState> = {
+    todos: todoReducer,
+    filtro: filtroReducer
 }
