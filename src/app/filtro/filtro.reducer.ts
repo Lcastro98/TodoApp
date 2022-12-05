5a03281155f4ce63dde9c288749d60c027eba7b8
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-12-05 10:09:10 -0500

    Creando un segundo reducer - Filtro

---
+++ src/app/filtro/filtro.reducer.ts
@@ -0,0 +1,8 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import { setFiltro, filtrosValidos } from './filtro.actions';
+
+export const initialState: filtrosValidos = 'todos';
+
+export const filtroReducer = createReducer<filtrosValidos, Action>(initialState,
+    on(setFiltro, (state, { filtro }) => filtro)
+);
